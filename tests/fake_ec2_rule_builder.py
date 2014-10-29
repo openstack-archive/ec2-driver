98843957fc033310e9628a6af4764e55e74a8e93
cameron-r <crowshanbin@gmail.com>
2014-10-29 17:28:10 -0500

    Ed & Cam | Implement RuleComparator for checking equality of OpenStack and EC2 rules

---
+++ tests/fake_ec2_rule_builder.py
@@ -0,0 +1,43 @@
+from collections import namedtuple
+
+class FakeEC2RuleBuilder():
+
+    EC2Rule = namedtuple('EC2Rule', 'ip_protocol from_port to_port grants')
+    GroupOrCIDR = namedtuple('GroupOrCIDR', 'cidr_ip group_id')
+
+    def __init__(self):
+        self.ip_protocol = 'udp'
+        self.from_port = 1111
+        self.to_port = 3333
+        self.ip_range = '0.0.0.0/0'
+        self.allowed_security_group_id = None
+
+    @staticmethod
+    def an_ec2_rule():
+        return FakeEC2RuleBuilder()
+
+    def with_ip_protocol(self, ip_protocol):
+        self.ip_protocol = ip_protocol
+        return self
+
+    def with_from_port(self, from_port):
+        self.from_port = from_port
+        return self
+
+    def with_to_port(self, to_port):
+        self.to_port = to_port
+        return self
+
+    def with_ip_range(self, ip_range):
+        self.ip_range = ip_range
+        self.allowed_security_group_id = None
+        return self
+
+    def with_allowed_security_group_id(self, allowed_security_group_id):
+        self.allowed_security_group_id = allowed_security_group_id
+        self.ip_range = None
+        return self
+
+    def build(self):
+        grants = [self.GroupOrCIDR(self.ip_range, self.allowed_security_group_id)]
+        return self.EC2Rule(self.ip_protocol, self.from_port, self.to_port, grants)
