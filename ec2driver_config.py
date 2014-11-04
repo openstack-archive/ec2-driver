e2723b37cfd660141241f48bbf01382796ff3b53
Venu Murthy <venu.murthy@gmail.com>
2014-11-04 17:33:49 +0530

    Delete ec2driver_config.py

--- ec2driver_config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-if os.environ.get('MOCK_EC2'):
-    print "test environment"
-    from ec2driver_test_config import *
-else:
-    print "prod env"
-    from ec2driver_standard_config import *
