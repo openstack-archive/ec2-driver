d1265791817225a72f9485905d05c3b638226204
cameron-r <crowshanbin@gmail.com>
2014-11-04 11:35:04 -0600

    Cameron & Ed | Test EC2 rule transformer

--- ec2_rule_transformer.py
+++ ec2_rule_transformer.py
@@ -8,11 +8,13 @@ class EC2RuleTransformer:
         self.ec2_connection = ec2_connection
 
     def to_rule(self, ec2_rule):
-        rule_args = deepcopy(ec2_rule)
+        rule_args = deepcopy(vars(ec2_rule))
+        del rule_args['grants']
 
         if ec2_rule.grants[0].cidr_ip:
             rule_args['ip_range'] = ec2_rule.grants[0].cidr_ip
         else:
             group_id = ec2_rule.grants[0].group_id
-            rule_args['group_name'] = self.ec2_connection.get_all_security_groups(group_ids=group_id)[0]
-        return Rule(**ec2_rule)
+            rule_args['group_name'] = self.ec2_connection.get_all_security_groups(group_ids=group_id)[0].name
+
+        return Rule(**rule_args)
