87d5806d47689527be8ef1572ce2a358db119a01
new <venu.murthy@gmail.com>
2014-11-28 17:44:50 +0530

    Venu | Updated License

--- ec2driver_test_config.py
+++ ec2driver_test_config.py
@@ -1,4 +1,5 @@
-# Copyright (c) 2014 Thoughtworks.
+# Copyright (c) 2014 ThoughtWorks
+# All Rights Reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -8,12 +9,11 @@
 #
 #    Unless required by applicable law or agreed to in writing, software
 #    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#    WARRANTIES OR CONDITIONS OF ANY KIND, either expressed or implied. See the
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
-#    under the License.
-
-# This is the config file which is going to hold the values for being able
+#    under the License.ng to hold the values for being able
 # to connect to the AWS Public cloud.
+
 from collections import defaultdict
 
 aws_region = "moto_region"
